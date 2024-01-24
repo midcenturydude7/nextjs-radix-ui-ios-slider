151267cc22f9591e3bb041bec5e90c548b90edb5
midCenturyDude7 <mgriffes@gmail.com>
2024-01-23 21:04:16 -0800

    Clamped range of slider value from 0 to 100

--- app/slider.tsx
+++ app/slider.tsx
@@ -4,9 +4,18 @@ import React from "react";
 import { SpeakerWaveIcon, SpeakerXMarkIcon } from "@heroicons/react/20/solid";
 import * as RadixSlider from "@radix-ui/react-slider";
 
-export default function Slider({ name }: { name?: string }) {
-  const [isUsingPointer, setIsUsingPointer] = React.useState(false);
-  const [internalValue, setInternalValue] = React.useState(50);
+export default function Slider({
+  name,
+  min = 0,
+  max = 100,
+}: {
+  name?: string;
+  min?: number;
+  max?: number;
+}) {
+  let [isUsingPointer, setIsUsingPointer] = React.useState(false);
+  let [internalValue, setInternalValue] = React.useState(50);
+  let [stash, setStash] = React.useState({ clientX: 0, internalValue });
 
   return (
     <div>
@@ -14,11 +23,27 @@ export default function Slider({ name }: { name?: string }) {
         <SpeakerXMarkIcon className="size-5 transition group-hover:scale-125 group-hover:text-white" />
         <RadixSlider.Root
           value={[internalValue]}
-          onValueChange={([v]) => setInternalValue(v)}
+          // onValueChange={([v]) => setInternalValue(v)}
+          min={min}
+          max={max}
           defaultValue={[50]}
           className="relative flex h-1.5 grow items-center transition-[height] group-hover:h-4"
           name={name}
-          onPointerDown={() => setIsUsingPointer(true)}
+          onPointerDown={(e) => {
+            setStash({ clientX: e.clientX, internalValue });
+            setIsUsingPointer(true);
+          }}
+          onPointerMove={(e) => {
+            if (e.buttons > 0) {
+              let diffInPixels = e.clientX - stash.clientX;
+              let sliderWidth = e.currentTarget.clientWidth;
+              let pixelsPerUnit = (max - min) / sliderWidth;
+              let diffInUnits = diffInPixels * pixelsPerUnit;
+              let newValue = stash.internalValue + diffInUnits;
+
+              setInternalValue(clamp(newValue, min, max));
+            }
+          }}
           onBlur={() => setIsUsingPointer(false)}
         >
           <RadixSlider.Track
@@ -39,4 +64,8 @@ export default function Slider({ name }: { name?: string }) {
       <p className="mt-4">{internalValue}</p>
     </div>
   );
+}
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
 }
